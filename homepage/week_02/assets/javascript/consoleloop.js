08c621ada410ac71672cbf1fe392899a1e5c4ced
EstherKlingbiel <kline420@newschool.edu>
2017-09-22 13:45:54 -0400

    Add files via upload

---
+++ homepage/week_02/assets/javascript/consoleloop.js
@@ -0,0 +1,29 @@
+var allWords = ['Anthropocentricities', 'Archaeoastronomies', 'Atrabiliousnesses', 'Bloodguiltinesses', 'Bloodthirstinesses', 'Buckminsterfullerene', 'Cantankerousnesses', 'Crashworthinesses', 'Crystallographically', 'Demythologization', 'Denominationalisms', 'Departmentalizations', 'Evapotranspiration', 'Everlastingnesses', 'Extemporaneousnesses', 'Feeblemindednesses', 'Floriferousnesses', 'Forethoughtfulnesses']
+
+console.log();
+for (var i = 0; i < allWords.length; i++) {
+	if (allWords[i].length / 4 === 5) {
+
+		console.log(allWords[i]);
+
+	} 
+} 
+
+console.log(' ');
+for (var i = 0; i < allWords.length; i++) {
+	if (allWords[i].length / 3 === 6) {
+		
+		console.log(allWords[i]);
+	}
+}
+
+console.log(' ');
+for (var i = 0; i < allWords.length; i++) {
+	if (allWords[i].length % 2 !== 0) {
+
+		console.log(allWords[i]);
+	}
+}
+
+console.log(' ');
+console.log('What pattern/patterns do you notice?');
